05ec9a8dfd22ab8ecb89afe4329a8c914037da7a
gamerd <daniil.kvasnikov@gmail.com>
2020-04-12 12:34:58 +0300

    GenerateRubik

--- Assets/Scripts/RubikGenerator.cs
+++ Assets/Scripts/RubikGenerator.cs
@@ -5,40 +5,42 @@ using UnityEngine;
 public class RubikGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
-    [SerializeField] private int size = 3;
+
+    private const int Size = 3;
 
     public List<GameObject> cubes;
     public List<CubeElement> cubeElements;
-
+    
     private void Awake()
     {
         Generate();
     }
-
+    
+    [ContextMenu("GenerateRubik")]
     private void Generate()
     {
         cubes = new List<GameObject>();
         cubeElements= new List<CubeElement>();
         var positionParent = transform.position;
         var transformParent = transform;
-        for (int x = 0; x < size; x++)
+        for (int x = 0; x < Size; x++)
         {
-            for (int y = 0; y < size; y++)
+            for (int y = 0; y < Size; y++)
             {
-                for (int z = 0; z < size; z++)
+                for (int z = 0; z < Size; z++)
                 {
                     var position = positionParent + new Vector3(x, y, z);
                     var cube = Instantiate(prefab, position, Quaternion.identity, transformParent);
                     cubes.Add(cube);
                     
                     var cubeElement = cube.GetComponent<CubeElement>();
-                    cubeElement.side[SideOfTheCube.FRONT].active = (x == size - 1);
-                    cubeElement.side[SideOfTheCube.BACK].active = (x == 0);
-                    cubeElement.side[SideOfTheCube.UP].active = (y == size - 1);
-                    cubeElement.side[SideOfTheCube.DOWN].active = (y == 0);
-                    cubeElement.side[SideOfTheCube.LEFT].active = (z == 0);
-                    cubeElement.side[SideOfTheCube.RIGHT].active = (z == size - 1);
                     if (cubeElement == null) throw new Exception("CubeElement not found");
+                    cubeElement.side[SideOfTheCube.FRONT].SetActive(x == Size - 1);
+                    cubeElement.side[SideOfTheCube.BACK].SetActive(x == 0);
+                    cubeElement.side[SideOfTheCube.UP].SetActive(y == Size - 1);
+                    cubeElement.side[SideOfTheCube.DOWN].SetActive(y == 0);
+                    cubeElement.side[SideOfTheCube.LEFT].SetActive(z == 0);
+                    cubeElement.side[SideOfTheCube.RIGHT].SetActive(z == Size - 1);
                     cubeElements.Add(cubeElement);
                 }
             }
